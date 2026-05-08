d5854ff477c7d2494f61555265020ea1e7b34c83
CharlesGit30 <charlesgit30@gmail.com>
2026-05-08 08:55:43 -0300

    Atualizar o lIstar_medicamentos.php

--- lIstar_medicamentos.php
+++ lIstar_medicamentos.php
@@ -61,6 +61,19 @@ $resultado = $conexao->query("SELECT * FROM medicamentos ORDER BY nome DESC");
     </table>
 </div>
 
+
+<div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+        <div class="vw-plugin-top-wrapper"></div>
+    </div>
+</div>
+<script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+<script>
+new window.VLibras.Widget('https://vlibras.gov.br/app');
+</script>
+
+
 </body>
 </html>
 
